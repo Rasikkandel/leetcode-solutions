e63fc56ac357d02ffde833ccb69156ad1ab9b847
Rasikkandel <110335530+Rasikkandel@users.noreply.github.com>
2026-08-29 20:35:39 +0545

    Time: 127 ms (18.28%), Space: 35.5 MB (6.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,18 +1,17 @@
 class Solution:
-     def rotate(self, nums: list[int], k: int) -> None:
+    def reverse(self , nums , start , end) -> None : 
+        while start < end : 
+            nums[start] , nums[end] = nums[end] , nums[start] 
+            start += 1 
+            end -= 1 
+        
+    def rotate(self, nums: list[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
         k = k % n  
-        arr = [] 
-        # first k lae naya array ma store
-        for i in range(n-k,n) :  
-            arr.append(nums[i]) 
-        # then rotate gareko pugni samma 
-        for i in range(n-k-1,-1,-1) : 
-            nums[i+k] = nums[i] 
-        # tespaxi adding the array element laskarai 
-        for i in range(k) : 
-            nums[i] = arr[i] 
+        self.reverse(nums, 0 , n-1) 
+        self.reverse(nums , 0 , k-1) 
+        self.reverse(nums , k , n-1) 
          
