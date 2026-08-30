f8cd2ceae10adc3e450a44b999dc07d04f4675f4
Rasikkandel <110335530+Rasikkandel@users.noreply.github.com>
2026-08-30 17:11:02 +0545

    Time: 3 ms (45.89%), Space: 19.4 MB (95.5%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -2,49 +2,24 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         m = len(nums1) 
         n = len(nums2) 
-        median_idx = (m+n) // 2 
-        i = 0 
-        j = 0 
-        idx = 0 
-        median = 0 
-        bhettiyo = 0 
-        temp = [] 
-        while(i < m and j < n) : 
-            if(nums1[i] < nums2[j]) :
-                temp.append(nums1[i])  
-                if(idx == median_idx) : 
-                    bhettiyo = 1 
-                    break 
-                idx += 1 
-                i += 1
+        total = m + n 
+        median_idx = total // 2 
+        i = j = idx = 0 
+        prev = current = 0 
 
+        while idx <= median_idx : 
+            if i < m and (  j >= n or nums1[i] < nums2[j]) : 
+                prev , current = current , nums1[i] 
+                i += 1 
             else : 
-                temp.append(nums2[j]) 
-                if(idx == median_idx) : 
-                    bhettiyo = 1
-                    break 
-                idx += 1  
+                prev , current = current , nums2[j] 
                 j += 1 
-        
-        while(i < m and bhettiyo == 0 ) : 
-            temp.append(nums1[i])  
-            if(idx == median_idx) : 
-                break 
             idx += 1 
-            i += 1
         
-        while(j < n and bhettiyo == 0) : 
-            temp.append(nums2[j])  
-            if(idx == median_idx) : 
-                break 
-            idx += 1 
-            j += 1
-
-
-        if((m+n)%2 != 0) : 
-            return temp[-1]
-        else : 
-            return (temp[-1] + temp[-2]) / 2
+        if total % 2 : 
+            return float(current) 
+        
+        return (current + prev) / 2
             
             
         
