ae28fcccf47ee18d38e46924419beb8dfc2bb092
Rasikkandel <110335530+Rasikkandel@users.noreply.github.com>
2026-09-03 09:42:57 +0545

    Time: 68 ms (53.66%), Space: 35.2 MB (85.98%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        min_ele = nums1[0]
+        even_count = 0 
+        odd_count = 0 
+        n = len(nums1)
+        for i in nums1 : 
+            if(i%2==0) : 
+                even_count += 1 
+            else : 
+                odd_count += 1
+            if(i < min_ele) : 
+                min_ele = i 
+        
+        if(even_count == n or odd_count == n) : 
+            return True 
+        
+        elif(min_ele % 2 == 1) : 
+            return True
+        return False
+
+
+        
