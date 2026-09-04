ecbeeb7f2d8080b11880c722b88912a5f76168ab
Rasikkandel <110335530+Rasikkandel@users.noreply.github.com>
2026-09-04 10:36:03 +0545

    Time: 7 ms (48.03%), Space: 19.2 MB (93.1%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -1,20 +1,21 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-        n = len(nums)
-        min_stable = n 
+        n = len(nums) 
+        prefix_max = [0]*n 
+        suffix_min = [0]*n 
+        prefix_max[0] = nums[0] 
+        for i in range(1,n) : 
+            prefix_max[i] = max(prefix_max[i-1] , nums[i]) 
+        
+        suffix_min[-1] = nums[-1] 
+        for i in range(n-2,-1,-1) : 
+            suffix_min[i] = min(suffix_min[i+1] , nums[i]) 
+
         for i in range(n) : 
-            max_prefix = 0 
-            min_suffix = float('inf') 
-            for j in range(i+1) : 
-                max_prefix = max(nums[j] , max_prefix) 
-            for j in range(i,n) : 
-                min_suffix = min(min_suffix , nums[j]) 
-            score = max_prefix - min_suffix 
-            if( i < min_stable and score <= k) : 
-                min_stable = i 
-        if(min_stable == n) : 
-            return -1 
-        return min_stable 
+            score = prefix_max[i] - suffix_min[i] 
+            if(score <= k) : 
+                return i 
+        return -1
              
 
             
