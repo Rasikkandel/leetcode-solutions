89f908136124ace803a47a42279f19133004f6ef
Rasikkandel <110335530+Rasikkandel@users.noreply.github.com>
2026-09-05 10:33:32 +0545

    Refactor longest consecutive sequence solution
    Refactor longestConsecutive function with multiple approaches including brute force, sorted array, and optimal set-based solution.

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,3 +1,38 @@
+""" Brute solution :: 
+def longestConsecutive(self, nums):
+        if not nums : 
+            return 0 
+        longest = 1 
+        for i in range(len(nums)) : 
+            current_number = nums[i] 
+            current_streak = 1 
+            while (current_number + 1) in nums : 
+                current_number += 1 
+                current_streak += 1 
+            longest = max(current_streak , longest) 
+        return longest
+        """ 
+"""
+## better 
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums : 
+            return 0 
+        nums.sort() 
+        n = len(nums) 
+        count = 1  
+        longest = 1  
+        for i in range(1,n) : 
+            if(nums[i] ==  nums[i-1] + 1) : 
+                count += 1 
+            elif (nums[i] == nums[i-1]) : 
+                continue 
+            else : 
+                count = 1 
+            longest = max(count , longest) 
+        return longest """
+
+#optimal
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         nums_set = set(nums)  
