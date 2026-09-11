84f171f0d248e3c155ba54382d652bab75f33d34
Rasikkandel <110335530+Rasikkandel@users.noreply.github.com>
2026-09-11 16:13:52 +0545

    Time: 16 ms (61.32%), Space: 19.3 MB (72.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -4,17 +4,22 @@ class Solution:
         count = 0 
         unique = set() 
         for i in range(n) : 
+            if digits[i] == 0 : 
+                continue
             for j in range(n) : 
+                if i == j : 
+                    continue 
                 for k in range(n) : 
-                    if j != i and k != j and i != k: 
-                        if digits[i] != 0 : 
-                            num = [digits[i] , digits[j] , digits[k] ] 
-                            number = int("".join(map(str, num)))  
-                            if number % 2 == 0 and number not in unique: 
-                                count += 1 
-                                unique.add(number)
-                            
-        return count
+                    if i == k or j == k or digits[k] % 2 != 0: 
+                        continue 
+                    number = digits[i]*100 + digits[j]*10 + digits[k] 
+                    if number not in unique : 
+                        unique.add(number) 
+                        count += 1
+                      
+        return count 
+
+
 
 
 
